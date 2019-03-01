49e711693db3e223ca5da25e00169bc7620edb71
nurblant <alexei.tzap@yandex.ru>
2019-03-01 23:24:42 +0500

    Update snippet.evolution-parser.php

--- assets/snippets/evolution-parser/snippet.evolution-parser.php
+++ assets/snippets/evolution-parser/snippet.evolution-parser.php
@@ -15,6 +15,8 @@ include_once ($modx->config["base_path"].'assets/snippets/evolution-parser/libs/
 include_once ($modx->config["base_path"].'assets/snippets/evolution-parser/libs/functions.php');
 
 $autorsList = 'http://zonanot.ru/notespiano/classika';
+$start = 0;
+$limit = 5;
 // curl запрос
 $ch = curl_init(); 
 curl_setopt($ch, CURLOPT_URL, trim($autorsList)); 
@@ -27,82 +29,105 @@ curl_close($ch);
 // парсинг
 $autorsListDocument = phpQuery::newDocument($autorsListPage);
 $a_listUrls = $autorsListDocument->find('div.attacments a');
+$i = 0;
 foreach($a_listUrls as $a_urlAutor) {
-  $domain = 'http://zonanot.ru';
-  $pqaurl_autor = pq($a_urlAutor);
-  $urlAutor = $domain.$pqaurl_autor->attr('href');
-  echo $urlAutor;
-  $template = 4;
-  $parent = 1;
-  $publiched = 1;
-  $media_dir = 'assets/files/';
-
-  // curl запрос
-  $ch = curl_init(); 
-  curl_setopt($ch, CURLOPT_URL, trim($urlAutor)); 
-  curl_setopt($ch, CURLOPT_HEADER, false); 
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30); 
-  $autorPage = curl_exec($ch); 
-  curl_close($ch);
-
-
-  // парсинг
-  $autorDocument = phpQuery::newDocument($autorPage);
-  $a_urls = $autorDocument->find('div.attacments a.mod-articles-category-title');
-
-  foreach($a_urls as $a_url) {
-
-    $pqaurl = pq($a_url);
-    echo $domain.($pqaurl->attr('href')).'<br>';
+  if(i>=$start && i<($start+$limit)) {
+    $domain = 'http://zonanot.ru';
+    $pqaurl_autor = pq($a_urlAutor);
+    $urlAutor = $domain.$pqaurl_autor->attr('href');
+    echo $urlAutor;
+    $template = 4;
+    $parent = 1;
+    $publiched = 1;
+    $media_dir = 'assets/files/';
 
     // curl запрос
     $ch = curl_init(); 
-    curl_setopt($ch, CURLOPT_URL, trim($domain.($pqaurl->attr('href')))); 
+    curl_setopt($ch, CURLOPT_URL, trim($urlAutor)); 
     curl_setopt($ch, CURLOPT_HEADER, false); 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30); 
-    $notePage = curl_exec($ch); 
+    $autorPage = curl_exec($ch); 
     curl_close($ch);
-    $noteDocument = phpQuery::newDocument($notePage);
-    $pageTitle = $noteDocument->find('.page-header h1')->text();
 
-    $downloadUrl = $noteDocument->find('#noti a')->attr('href');
-    $autorName = $noteDocument->find('.breadcrumbs a:last')->text();
-    
-      $doc = new CakeMODx;
-      $fields = array(
-        'pagetitle' => $pageTitle,
-        'template' => $template,
-        'parent' => $parent,
-        'published' => $publiched,
-        //'link_attributes' => $link,
-        //'menutitle' => $strn
-      );
-      $id = $doc->newDocument($fields);
 
-      if ($id) {
-        echo 'Документ создан '.$id;
-        $alias = $trans->stripAlias($pageTitle, $char_restrict, $word_separator);
-        // перезапись ресурса
+    // парсинг
+    $autorDocument = phpQuery::newDocument($autorPage);
+    $a_urls = $autorDocument->find('div.attacments a.mod-articles-category-title');
+
+    foreach($a_urls as $a_url) {
+
+      $pqaurl = pq($a_url);
+      echo $domain.($pqaurl->attr('href')).'<br>';
+
+      // curl запрос
+      $ch = curl_init(); 
+      curl_setopt($ch, CURLOPT_URL, trim($domain.($pqaurl->attr('href')))); 
+      curl_setopt($ch, CURLOPT_HEADER, false); 
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+      curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30); 
+      $notePage = curl_exec($ch); 
+      curl_close($ch);
+      $noteDocument = phpQuery::newDocument($notePage);
+      $pageTitle = $noteDocument->find('.page-header h1')->text();
+
+      $downloadUrl = $noteDocument->find('#noti a')->attr('href');
+      $autorName = $noteDocument->find('.breadcrumbs a:last')->text();
+      
+        $doc = new CakeMODx;
         $fields = array(
-          'alias' => $id.'-'.$alias,
-          //'pagetitle' => $pagetitle,
-          'content' => $pageTitle.' - ноты для фортепиано автора '.$autorName.' ',
+          'pagetitle' => $pageTitle,
+          'template' => $template,
+          'parent' => $parent,
+          'published' => $publiched,
+          //'link_attributes' => $link,
+          //'menutitle' => $strn
         );
-        $doc->updateDocument($id,$fields);
-        //echo $domain.$downloadUrl;
-        $doc->setTV(5, $id, $autorName);
-        if(substr($downloadUrl, -3, 3) == 'pdf') {
-          $doc->setTV(6, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
+        $id = $doc->newDocument($fields);
+
+        if ($id) {
+          echo 'Документ создан '.$id;
+          $alias = $trans->stripAlias($pageTitle, $char_restrict, $word_separator);
+          // перезапись ресурса
+          $fields = array(
+            'alias' => $id.'-'.$alias,
+            //'pagetitle' => $pagetitle,
+            'content' => $pageTitle.' - ноты для фортепиано автора '.$autorName.' ',
+          );
+          $doc->updateDocument($id,$fields);
+          //echo $domain.$downloadUrl;
+          $doc->setTV(5, $id, $autorName);
+          if(substr($downloadUrl, -3, 3) == 'pdf') {
+            $doc->setTV(6, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
+          }
+          if(substr($downloadUrl, -3, 3) == 'doc') {
+            $doc->setTV(10, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
+          } 
+          if(substr($downloadUrl, -3, 3) == 'zip') {
+            $doc->setTV(9, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
+          } 
+          if(substr($downloadUrl, -3, 3) == 'rar') {
+            $doc->setTV(9, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
+          } 
+          if(substr($downloadUrl, -4, 4) == 'jpeg') {
+            $doc->setTV(7, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
+          } 
+          if(substr($downloadUrl, -4, 4) == 'JPEG') {
+            $doc->setTV(7, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
+          } 
+          if(substr($downloadUrl, -3, 3) == 'png') {
+            $doc->setTV(7, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
+          } 
+          if(substr($downloadUrl, -3, 3) == 'jpg') {
+            $doc->setTV(7, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
+          } 
+          $doc->updateCache();
         }
-        if(substr($downloadUrl, -3, 3) == 'doc') {
-          $doc->setTV(10, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
-        } 
-        $doc->updateCache();
-      }
-    
-  } 
+      
+    } 
+  }
+  i++;
+  
 }
     
 return $output;
