9b7d79238e1e011849cc7b087e3bd485fe7150fc
nurblant <alexei.tzap@yandex.ru>
2019-03-19 22:28:23 +0500

    Update snippet.evolution-parser.php

--- assets/snippets/evolution-parser/snippet.evolution-parser.php
+++ assets/snippets/evolution-parser/snippet.evolution-parser.php
@@ -28,7 +28,7 @@ curl_close($ch);
 
 // парсинг
 $autorsListDocument = phpQuery::newDocument($autorsListPage);
-$a_listUrls = $autorsListDocument->find('li.menu-item a');
+$a_listUrls = $autorsListDocument->find('.entry-content li.menu-item a');
 echo "<pre>".$a_listUrls."</pre>";
 $iY = 0;
 foreach($a_listUrls as $a_urlAutor) {
@@ -56,76 +56,88 @@ foreach($a_listUrls as $a_urlAutor) {
     $autorDocument = phpQuery::newDocument($autorPage);
     $a_urls = $autorDocument->find('li.menu-item a');
 
-    foreach($a_urls as $a_url) {
+    $pageTitle = $noteDocument->find('.entry-content li.menu-item a')->text();
+    $doc = new CakeMODx;
+    $fields = array(
+      'pagetitle' => $pageTitle,
+      'template' => $template,
+      'parent' => $parent,
+      'published' => $publiched,
+      //'link_attributes' => $link,
+      //'menutitle' => $strn
+    );
+    $id = $doc->newDocument($fields);
 
-      $pqaurl = pq($a_url);
-      echo $domain.($pqaurl->attr('href')).'<br>';
+    // foreach($a_urls as $a_url) {
 
-      // curl запрос
-      $ch = curl_init(); 
-      curl_setopt($ch, CURLOPT_URL, trim($domain.($pqaurl->attr('href')))); 
-      curl_setopt($ch, CURLOPT_HEADER, false); 
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-      curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30); 
-      $notePage = curl_exec($ch); 
-      curl_close($ch);
-      $noteDocument = phpQuery::newDocument($notePage);
-      $pageTitle = $noteDocument->find('.page-header h1')->text();
+    //   $pqaurl = pq($a_url);
+    //   echo $domain.($pqaurl->attr('href')).'<br>';
 
-      $downloadUrl = $noteDocument->find('#noti a')->attr('href');
-      $autorName = $noteDocument->find('.breadcrumbs a:last')->text();
+    //   // curl запрос
+    //   $ch = curl_init(); 
+    //   curl_setopt($ch, CURLOPT_URL, trim($domain.($pqaurl->attr('href')))); 
+    //   curl_setopt($ch, CURLOPT_HEADER, false); 
+    //   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+    //   curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30); 
+    //   $notePage = curl_exec($ch); 
+    //   curl_close($ch);
+    //   $noteDocument = phpQuery::newDocument($notePage);
+    //   $pageTitle = $noteDocument->find('.page-header h1')->text();
+
+    //   // $downloadUrl = $noteDocument->find('#noti a')->attr('href');
+    //   // $autorName = $noteDocument->find('.breadcrumbs a:last')->text();
       
-        $doc = new CakeMODx;
-        $fields = array(
-          'pagetitle' => $pageTitle,
-          'template' => $template,
-          'parent' => $parent,
-          'published' => $publiched,
-          //'link_attributes' => $link,
-          //'menutitle' => $strn
-        );
-        $id = $doc->newDocument($fields);
+    //     $doc = new CakeMODx;
+    //     $fields = array(
+    //       'pagetitle' => $pageTitle,
+    //       'template' => $template,
+    //       'parent' => $parent,
+    //       'published' => $publiched,
+    //       //'link_attributes' => $link,
+    //       //'menutitle' => $strn
+    //     );
+    //     $id = $doc->newDocument($fields);
 
-        // if ($id) {
-        //   echo 'Документ создан '.$id;
-        //   $alias = $trans->stripAlias($pageTitle, $char_restrict, $word_separator);
-        //   // перезапись ресурса
-        //   $fields = array(
-        //     'alias' => $id.'-'.$alias,
-        //     //'pagetitle' => $pagetitle,
-        //     'content' => $pageTitle.' - инструкция по применению',
-        //   );
-        //   $doc->updateDocument($id,$fields);
-        //   //echo $domain.$downloadUrl;
-        //   //$doc->setTV(6, $id, $autorName);
-        //   // if(substr($downloadUrl, -3, 3) == 'pdf') {
-        //   //   $doc->setTV(7, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
-        //   // }
-        //   // if(substr($downloadUrl, -3, 3) == 'doc') {
-        //   //   $doc->setTV(13, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
-        //   // } 
-        //   // if(substr($downloadUrl, -3, 3) == 'zip') {
-        //   //   $doc->setTV(8, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
-        //   // } 
-        //   // if(substr($downloadUrl, -3, 3) == 'rar') {
-        //   //   $doc->setTV(8, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
-        //   // } 
-        //   // if(substr($downloadUrl, -4, 4) == 'jpeg') {
-        //   //   $doc->setTV(5, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
-        //   // } 
-        //   // if(substr($downloadUrl, -4, 4) == 'JPEG') {
-        //   //   $doc->setTV(5, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
-        //   // } 
-        //   // if(substr($downloadUrl, -3, 3) == 'png') {
-        //   //   $doc->setTV(5, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
-        //   // } 
-        //   // if(substr($downloadUrl, -3, 3) == 'jpg') {
-        //   //   $doc->setTV(5, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
-        //   // } 
-        //   $doc->updateCache();
-        // }
+    //     // if ($id) {
+    //     //   echo 'Документ создан '.$id;
+    //     //   $alias = $trans->stripAlias($pageTitle, $char_restrict, $word_separator);
+    //     //   // перезапись ресурса
+    //     //   $fields = array(
+    //     //     'alias' => $id.'-'.$alias,
+    //     //     //'pagetitle' => $pagetitle,
+    //     //     'content' => $pageTitle.' - инструкция по применению',
+    //     //   );
+    //     //   $doc->updateDocument($id,$fields);
+    //     //   //echo $domain.$downloadUrl;
+    //     //   //$doc->setTV(6, $id, $autorName);
+    //     //   // if(substr($downloadUrl, -3, 3) == 'pdf') {
+    //     //   //   $doc->setTV(7, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
+    //     //   // }
+    //     //   // if(substr($downloadUrl, -3, 3) == 'doc') {
+    //     //   //   $doc->setTV(13, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
+    //     //   // } 
+    //     //   // if(substr($downloadUrl, -3, 3) == 'zip') {
+    //     //   //   $doc->setTV(8, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
+    //     //   // } 
+    //     //   // if(substr($downloadUrl, -3, 3) == 'rar') {
+    //     //   //   $doc->setTV(8, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
+    //     //   // } 
+    //     //   // if(substr($downloadUrl, -4, 4) == 'jpeg') {
+    //     //   //   $doc->setTV(5, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
+    //     //   // } 
+    //     //   // if(substr($downloadUrl, -4, 4) == 'JPEG') {
+    //     //   //   $doc->setTV(5, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
+    //     //   // } 
+    //     //   // if(substr($downloadUrl, -3, 3) == 'png') {
+    //     //   //   $doc->setTV(5, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
+    //     //   // } 
+    //     //   // if(substr($downloadUrl, -3, 3) == 'jpg') {
+    //     //   //   $doc->setTV(5, $id, saveMedia($domain.$downloadUrl, $alias, $id, $media_dir));
+    //     //   // } 
+    //     //   $doc->updateCache();
+    //     // }
       
-    } 
+    // } 
   }
   $iY = $iY+1;
   
